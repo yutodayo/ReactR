9ebf56bae6be4e119b7cd6992bf89b2275c47275
yutodayo <50805424+yutodayo@users.noreply.github.com>
2021-06-12 23:42:25 +0900

    history

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,12 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(10).keys()];
+  const history = useHistory();
+  const onClickDetailA = () => history.push("/page1/detailA");
   return (
     <div>
       <h1>Page1ページです</h1>
-      <Link to="/page1/detailA">DetailA</Link>
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,13 @@
+import { useHistory, useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+  const history = useHistory();
+  const onClickBack = () => history.goBack();
   return (
     <div>
       <h1>Page1DetailAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
